90eb5b227b64ad9aa5c3a70aaa26bb7d60ba0e62
Rafayel Gardishyan <rgardishyan@gmail.com>
2018-07-02 17:58:29 +0200

    Create print_input_vars_comments.py

---
+++ voorbeelden/print_input_vars_comments.py
@@ -0,0 +1,32 @@
+print("Hallo, Wereld!") # Dit zal Hallo, Wereld laten zien op het scherm
+
+variabele = "Dit is een vraiabele" # Dit geeft de naam "variabele" de waarde "die is een variabele"
+
+print(variabele) # Dit zal de waarde van variabele variabele uitprinten
+
+variabele2 = input("Toets wat in: ") # Dit geeft de naam "variabele2" de waarde van wat de gebruiker heeft ingevoerd
+
+print(variabele2) # Dit zal de waarde van variabele variabele2 uitprinten
+
+variabele = variabele2 # Dit zal de waarde van varuiabele variabele gelijkmaken met de waarde van variabele2
+
+print(variabele) # Dit zal de nieuwe waarde van variabele variabele uitprinten
+
+variabele3 = 2 # Dit geeft de naam "variabele3"  de waarde 2
+
+print(type(variabele)) # Dit zal de type van variabele variabele uitprinten (str = string)
+
+print(type(variabele3)) # Dit zal de type van variabele3 variabele uitprinten (int = integer)
+
+variabele = variabele3 # Dit zal de waarde van variabele variabele gelijkemaken met de waarde van variabele3
+
+print(type(variabele)) # Dit zal de nieuwe type van veriabele variabele uitprinten (int = integer)
+
+# print("Dit zal niet uitgevoerd worden") # Dit is een commen
+print("En dit zal wel weer uitgevoerd worden")
+
+""" # Dit is een meerregelig (multiline) comment
+Alle code hieronder zal niet uitgevoerd worden
+print("Hallo")
+var1 = input("Dit zal niet uitgevoerd worden")
+"""
